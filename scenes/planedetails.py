2218edd6123b34f00f09cb3b582dc8f94ac5d879
bdstps <bdstoops@gmail.com>
2023-05-07 11:48:46 -0600

    reinstating cycling

--- scenes/planedetails.py
+++ scenes/planedetails.py
@@ -8,11 +8,13 @@ PLANE_DETAILS_COLOUR = colours.WHITE
 PLANE_DISTANCE_FROM_TOP = 31
 PLANE_TEXT_HEIGHT = 6
 PLANE_FONT = fonts.small
+PLANE_DISPLAY_START_POSITION = 5
 
 class PlaneDetailsScene(object):
     def __init__(self):
         super().__init__()
         self.reset_scrolling()
+        self.foo = 0
         self._data_all_looped = False
 
     @Animator.KeyFrame.add(1)
@@ -37,21 +39,21 @@ class PlaneDetailsScene(object):
         text_length = graphics.DrawText(
             self.canvas,
             PLANE_FONT,
-            self.plane_position,
+            self.draw_position,
             PLANE_DISTANCE_FROM_TOP,
             PLANE_DETAILS_COLOUR,
             plane,
         )
 
         # Handle scrolling 
-        # self.plane_position = -1
-        # if self.plane_position + text_length < 0:
-        #     self.reset_scrolling()
-        #     if len(self._data) > 1:
-        #         self._data_index = (self._data_index + 1) % len(self._data)
-        #         self._data_all_looped = (not self._data_index) or self._data_all_looped
-        #         self.reset_scene()
+        self.foo += 1
+        if self.foo > 20:
+            self.reset_scrolling()
+            if len(self._data) > 1:
+                self._data_index = (self._data_index + 1) % len(self._data)
+                self._data_all_looped = (not self._data_index) or self._data_all_looped
+                self.reset_scene()
 
     @Animator.KeyFrame.add(0)
     def reset_scrolling(self):
-        self.plane_position = 5
+        self.foo = 0
