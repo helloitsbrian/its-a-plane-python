3cd539eb6f646946299df21b537a379406efebee
bdstps <bdstoops@gmail.com>
2023-05-11 16:41:17 -0600

    change to colors and fonts

--- scenes/journey.py
+++ scenes/journey.py
@@ -26,7 +26,7 @@ JOURNEY_SPACING = 16
 JOURNEY_FONT = fonts.large_bold
 JOURNEY_FONT_SELECTED = fonts.large_bold
 JOURNEY_COLOUR = colours.WHITE
-SELECTED_COLOUR = colours.YELLOW_DARK
+SELECTED_COLOUR = colours.YELLOW
 ARROW_COLOUR = colours.GREEN
 
 # Element Positions
